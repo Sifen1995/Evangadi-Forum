7d0bd3a4465ec2a824edd03f9c2dd9ddf543cd06
sifen getachew <sifenmesfin10@gmail.com>
2025-07-17 01:54:36 -0700

    api updated

--- Client/src/Api/axios.js
+++ Client/src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
     
-    baseURL: "https://forum-api-deploy-20.onrender.com/api",
+    // baseURL: "https://forum-api-deploy-20.onrender.com/api",
+    baseURL: "https://evangadiforum-be.kelilmohamad.com/api",
 });
 export default instance;
